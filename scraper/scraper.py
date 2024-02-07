267000a7b19cfa57b8db8bdb595fb2ff10086875
slvg01 <151031347+slvg01@users.noreply.github.com>
2024-02-07 18:21:30 +0100

    dev code rewiew/mods and output

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,6 +2,7 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 variable_dict = {'ID':["div","class", "classified__header--immoweb-code"],
                  'Price': ["span", "class", "sr-only"]}
@@ -30,15 +31,18 @@ class Immoweb_Scraper:
         self.variable_dict = variable_dict
         self.base_urls_list = []
         self.immoweb_urls_list = []
-        self.element_list = ["Bedrooms","Living area","Kitchen type","Furnished","Terrace surface","Garden surface","Number of frontages","Swimming pool","Building condition"]
+        self.element_list = ["Price", "Construction year","Bedrooms","Living area","Kitchen type","Furnished","Terrace surface", "Surface of the plot","Garden surface","Number of frontages","Swimming pool","Building condition"]
         self.data_set = []
+        self.dataset_df = pd.DataFrame()
         self.soups = []
+
+
         
     def get_base_urls(self):
         """
             Get the list of base URLs after applying the filter 
             as Life Annuity as False. Go through mupltiple pages 
-            to get the list of all base URLs which will help in 
+            to get the list of all base URLs which will allow  
             fetching 10000 URLs of House or Appartment for sale.
         """
         for i in range(1,2):
@@ -46,6 +50,8 @@ class Immoweb_Scraper:
             self.base_urls_list.append(base_url)
         print('Base URLs generated!')
         return(self.base_urls_list)    
+    
+
         
     def get_immoweb_urls(self):
         """
@@ -58,11 +64,13 @@ class Immoweb_Scraper:
             soup = BeautifulSoup(url_content, "html.parser")
             for tag in soup.find_all("a", attrs={"class" : "card__title-link"}):
                 immoweb_url = tag.get("href")
-                if "www.immoweb.be" in immoweb_url and counter < 10:
+                if "www.immoweb.be" in immoweb_url and counter < 5:
                     self.immoweb_urls_list.append(immoweb_url)
                     counter += 1
         print('Immoweb URLs generated!', len(self.immoweb_urls_list))
         return(self.immoweb_urls_list)
+    
+
 
     def request_urls(self):
         """
@@ -71,17 +79,22 @@ class Immoweb_Scraper:
         Sends HTTP requests to the provided URLs, parses the HTML content,
         and stores the parsed soup objects.
         """
+        self.immoweb_urls_list = self.get_immoweb_urls()
         for url in self.immoweb_urls_list:
-            r = requests.get(url)
-            if r.status_code == 200:
-                self.soups.append(BeautifulSoup(r.content, "html.parser"))
+            url_content = requests.get(url)
+            if url_content.status_code == 200:
+                self.soups.append(BeautifulSoup(url_content.content, "html.parser"))
             else:
                 continue
         print(len(self.soups))
+        return self.soups
+    
+               
 
     def scrape_table_dataset(self):
         """
-            Get the elements value from the table tag
+            Get the 1st part of the parameters from URLs extracting
+            and the 2nd part of the parameters from page scraping
         """
         self.immoweb_urls_list = self.get_immoweb_urls()
         for each_url in self.immoweb_urls_list:
@@ -91,78 +104,71 @@ class Immoweb_Scraper:
             data_dict["Locality name"] = each_url.split('/')[-3]
             data_dict["Postal code"] = each_url.split('/')[-2]
             data_dict["Type of property"] = each_url.split('/')[-5]
-            url_content = requests.get(each_url).content
-            soup = BeautifulSoup(url_content, "html.parser")
-            #print(each_url)
-            for tag in soup.find_all("tr", attrs={"class" : "classified-table__row"}):
-                for tag1 in tag.find_all("th", attrs={"class" : "classified-table__header"}):
-                    if tag1.string is not None:                
-                        #print(tag1.string.strip())
-                        for element in self.element_list:
-                            if element == tag1.string.strip():
-                                tag_text = str(tag.td).strip().replace("\n","").replace(" ","")
-                                #print(tag_text)
-                                start_loc = tag_text.find('>')
-                                end_loc = tag_text.find('<',tag_text.find('<')+1)
-                                table_data = tag_text[start_loc+1:end_loc]
-                                #print(element + ' : '+ table_data)
-                                data_dict[element] = table_data
-            #print(data_dict)
+            
+            
+            for soup in self.soups:
+                for tag in soup.find_all("tr", attrs={"class" : "classified-table__row"}):
+                    for tag1 in tag.find_all("th", attrs={"class" : "classified-table__header"}):
+                        if tag1.string is not None:                
+                            #print(tag1.string.strip())
+                            for element in self.element_list:
+                                if element == tag1.string.strip():
+                                    tag_text = str(tag.td).strip().replace("\n","").replace(" ","")
+                                    #print(tag_text)
+                                    start_loc = tag_text.find('>')
+                                    end_loc = tag_text.find('<',tag_text.find('<')+1)
+                                    table_data = tag_text[start_loc+1:end_loc]
+                                    #print(element + ' : '+ table_data)
+                                    data_dict[element] = table_data
             self.data_set.append(data_dict)
+        #print(self.data_set)
         return(self.data_set)
 
 
-    def scrape_vars(self):
-        """
-        Scrape data variables from parsed HTML content.
 
-        Scrape data variables specified in the variable dictionary from
-        the parsed HTML content and store them in lists.
-        """
-        self.listOfLists = [[] for _ in range(len(self.variable_dict))]
-        for i, (k, v) in enumerate(self.variable_dict.items()):
-            for soup in self.soups:
-                if soup:
-                    variable = soup.find(v[0], attrs={v[1]: v[2]})
-                    if variable:
-                        self.listOfLists[i].extend(elem for elem in variable)
-                    else:
-                        self.listOfLists[i].extend(None)
-        print('Scraping successful')
-        return self.listOfLists
+    def to_DataFrame (self) :
+        """ allow to convert the data_set list of dict in a DataFrame """
+        self.data_set_df = pd.DataFrame(self.data_set)
+        print(self.data_set_df.head(3))
+        return self.data_set_df     
+         
+
+
+    def to_csv (self):
+        """ allow to convert the data_set DataFrame in CSV """
+        data_set = self.data_set_df.to_csv ('data_set.csv', index= False)
+        return data_set
+
+
+        
+
+# Example usage and testing:
+immoscrap = Immoweb_Scraper(variable_dict)
+immoscrap.get_immoweb_urls()
+immoscrap.request_urls()
+immoscrap.scrape_table_dataset()
+immoscrap.to_DataFrame()
+immoscrap.to_csv()
+
+
+"""
+
+def save_csv(self):
     
-    def get_elements_value(self):
-        for soup in self.soups:
-            for tag in soup.find_all("th", attrs={"class" : "classified-table__header"}):
-                print(tag)
-        print('Got elements')
-
-    def to_dict(self):
-        """
-        Convert scraped data to a dictionary.
-
-        Convert the scraped data lists to a dictionary where keys are
-        variable names and values are corresponding lists.
-        """
-        self.immo_dict = dict(zip(self.variable_dict.keys(), self.listOfLists))
-        print('Data was successfully converted to dictionary!')
-        return self.immo_dict
-
-    def save_csv(self):
-        """
         Save scraped data dictionary to a CSV file.
 
         Save the scraped data dictionary to a CSV file where each key-value
         pair is written as a row with the key in the first column and the
         value in the second column.
-        """
+    
         with open('immo_dict.csv', 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
-            for key, value in self.immo_dict.items():
+            for key, value in self.data_set.items():
                 writer.writerow([key, value])
+   
 
-    def URL_extractor(self, url):
-        """
+def URL_extractor(self, url):
+        
         Extract data from URLs.
 
         Extract relevant data (IDs, Postal_codes, Locality_names, Subtypes)
@@ -173,7 +179,7 @@ class Immoweb_Scraper:
 
         Returns:
         - dict: A dictionary containing extracted data.
-        """
+        
         splits = url.split('/')
         IDs = splits[-1]
         Postal_code = splits[-2]
@@ -182,15 +188,17 @@ class Immoweb_Scraper:
 
         # Return the extracted data as a dictionary
         return {'ID': IDs, 'Postal_code': Postal_code, 'Locality_name': Locality_name, 'Subtype': Subtype}
-
+        
+        
+ 
     def extract_urls(self):
-        """
+        
         Extract data from URLs and save to a CSV file.
 
         Extract relevant data (IDs, Postal_codes, Locality_names, Subtypes)
         from the provided URLs, save the data to a dictionary, and then write
         the dictionary to a CSV file.
-        """
+        
         url_dict = {}
 
         IDs = []
@@ -216,17 +224,7 @@ class Immoweb_Scraper:
                 writer.writerow([key, value])
 
         return url_dict
-    
+          
+               
         
-
-# Example usage and testing:
-immoscrap = Immoweb_Scraper(variable_dict)
-immoscrap.get_immoweb_urls()
-immoscrap.request_urls()
-#immoscrap.scrape_vars()
-#immoscrap.to_dict()
-#immoscrap.save_csv()
-#immoscrap.get_elements_value()
-scraped_data = immoscrap.scrape_table_dataset()
-print(scraped_data)
-# immoscrap.extract_urls()
+        """
